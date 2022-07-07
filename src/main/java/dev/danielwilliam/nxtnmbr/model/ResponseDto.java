0a060e9b5788dfbb8fe76576976995b27b7daf18
dwi13L <dw22197@gmail.com>
2022-07-07 21:14:04 +0530

    Added Response DTO

---
+++ src/main/java/dev/danielwilliam/nxtnmbr/model/ResponseDto.java
@@ -0,0 +1,15 @@
+package dev.danielwilliam.nxtnmbr.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseDto {
+    private int OldValue;
+    private int NewValue;
+}

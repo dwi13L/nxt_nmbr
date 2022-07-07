f434103ebe9de267e754afc755d8f01a804f835d
dwi13L <dw22197@gmail.com>
2022-07-07 20:37:23 +0530

    initial commit

---
+++ src/main/java/dev/danielwilliam/nxtnmbr/NxtNmbrApplication.java
@@ -0,0 +1,13 @@
+package dev.danielwilliam.nxtnmbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxtNmbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxtNmbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/danielwilliam/nxtnmbr/NxtNmbrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielwilliam.nxtnmbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NxtNmbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
